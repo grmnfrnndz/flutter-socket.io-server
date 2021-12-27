67215fed8ca8e93729cdc855c7093da0226f988b
german contreras f <german.contreras.f@gmail.com>
2021-12-26 21:12:28 -0300

    fin seccion 3

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+// app de express
+const app = express();
+
+// node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/sockets');
+
+
+
+
+// path publico 
+const publicPath = path.resolve(__dirname, 'public'); 
+app.use(express.static(publicPath));
+
+
+server.listen(process.env.PORT, (error_x) => {
+    if (error_x) throw new Error(error_x);
+
+    console.log('Servidor corriendo en puerto !!!!: ', process.env.PORT);
+});
